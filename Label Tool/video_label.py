8d986939dc38ab60d5bcf9f99ba63e3bb7737c05
Kate Lo <xe870731@gmail.com>
2019-03-09 01:16:28 +0800

    remove type labeling

--- Label Tool/video_label.py
+++ Label Tool/video_label.py
@@ -2,11 +2,11 @@ import numpy as np
 import cv2
 import pickle
 from collections import defaultdict
+
 name="TAI Tzu Ying vs CHEN Yufei 2018 Indonesia Open Final  vs"
 ext=".mp4"
 filename=name+ext
 data_position=defaultdict(list)
-data_type=defaultdict(set)
 
 total_frame=0
 cap = cv2.VideoCapture(filename)
@@ -21,12 +21,14 @@ def toggle_set(myset,element):
 		myset.remove(element)
 	else:
 		myset.add(element)
+
 def showinfo(n):
-	global data_position,data_type
-	print(n,data_position[n],data_type[n])
+	global data_position
+	print(n,data_position[n])
+
 def click_and_crop(event, x, y, flags, param):
 	# grab references to the global variables
-	global data_position,data_type,cap,current
+	global data_position,cap,current
 	global image,count_map,court_map_base
 	
 	if event == cv2.EVENT_LBUTTONDOWN:
@@ -36,22 +38,26 @@ def click_and_crop(event, x, y, flags, param):
 		# data[current]= (x,y)
 		# current+=1
 		# image=toframe(cap,current,total_frame)
+
 	if event == cv2.EVENT_RBUTTONDOWN:
 		data_position[current]=[]
 		toframe(cap,current,total_frame)
 		# del data[current]
 		# image=toframe(cap,current,total_frame)
+
 	if event==cv2.EVENT_MOUSEWHEEL:
 		if flags>0:
 			current-=1
 		else:
 			current+=1
 		image=toframe(cap,current,total_frame)
+
 def toframe(cap,n,total_frame):
 	global court_map,court_map_base
 	showinfo(n)
 	cap.set(cv2.CAP_PROP_POS_FRAMES,n);
 	ret, frame = cap.read()
+
 	if not ret:
 		return None
 	else:
@@ -71,7 +77,6 @@ cv2.setMouseCallback("map", click_and_crop)
 # keep looping until the 'q' key is pressed
 while True:
 	# display the image and wait for a keypress
-	
 	cv2.imshow("image", cv2.resize(image,(0,0),fx=0.5,fy=0.5))
 	cv2.imshow("map", court_map)
 	key = cv2.waitKey(1) & 0xFF
@@ -92,21 +97,16 @@ while True:
 	if key == ord("y"):
 		current-=1
 		image=toframe(cap,current,total_frame)
-
 	if key == ord("k"):
 		try:
 			pickle.dump(data_position,open(name+"_position.pkl",'wb'))
-			pickle.dump(data_type,open(name+"_type.pkl",'wb'))
 			print ("saved to "+name+"_position.pkl")
-			print ("saved to "+name+"_type.pkl")
 		except Exception as e:
 			print (str(e))
 	if key == ord("l"):
 		try:
 			data_position=pickle.load(open(name+"_position.pkl",'rb'))
-			data_type=pickle.load(open(name+"_type.pkl",'rb'))
 			print ("load from "+name+"_position.pkl")
-			print ("load from "+name+"_type.pkl")
 			max_frame=max(list(data_position.keys()))
 			current=max_frame
 			toframe(cap,max_frame,total_frame)
@@ -130,7 +130,6 @@ while True:
 # if there are two reference points, then crop the region of interest
 # from teh image and display it
 
- 
 # close all open windows
 cv2.destroyAllWindows()
 cap.release()
