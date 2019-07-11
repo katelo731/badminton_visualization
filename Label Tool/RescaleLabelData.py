b0742c87bb30a48a245f37cf0e40fff966cc1694
Kate Lo <xe870731@gmail.com>
2019-07-11 16:41:22 +0800

    revise court to 3 by 3 grid

--- Label Tool/RescaleLabelData.py
+++ Label Tool/RescaleLabelData.py
@@ -2,7 +2,8 @@ import pickle
 import math
 import cv2
 from itertools import islice
-name = "2.Final - TAI Tzu Ying vs CHEN YuFei - Badminton Indonesia Open 2018"
+
+name = "2019亞錦賽-周天成VS石宇奇-24"
 ext=".mp4"
 filename=name+ext
 
@@ -52,191 +53,132 @@ for i in range(len(hit)):
 
 			#convert coordinate to area
 			if( y == 417):
-				if( x < 10):		
-					area = "F0"	
-				if( x >= 10 and x < 40):
-					area = "D0"
-				elif( x >= 40 and x < 195):				
+				if( x < 40):
+					area = "D0"	
+				if( x >= 40 and x < 105.1):
 					area = "B0"
-				elif( x >= 195 and x < 350):
+				elif( x >= 105.1 and x < 283.35):
 					area = "A0"
-				elif( x >= 350 and x <= 378):
+				elif( x >= 283.35 and x < 350):
 					area = "C0"
-				else:
+				elif( x >= 350):
 					area = "E0"
 			elif( y < 14):
-				if( x < 10):		
-					area = "F5"				
-				elif( x >= 10 and x < 40):	
-					area = "D5"
-				elif( x >= 40 and x < 195):
-					area = "B5"
-				elif( x >= 195 and x < 350):				
-					area = "A5"
-				elif( x >= 350 and x < 378):
-					area = "C5"
-				else:			
-					area = "E5"
-			elif( y > 820):
-				if( x < 10):
-					area = "E5"
-				if( x >= 10 and x < 40):			
-					area = "C5"				
-				elif( x >= 40 and x < 195):			
-					area = "A5"				
-				elif( x >= 195 and x < 350):		
-					area = "B5"				
-				elif( x >= 350 and x < 378):			
-					area = "D5"				
-				else:			
-					area = "F5"			
-			elif( x < 10):
-				if( y >= 14 and y < 60):		
-					area = "F4"				
-				elif( y >= 60 and y < 178):			
-					area = "F3"			
-				elif( y >= 178 and y < 296):
-					area = "F2"				
-				elif( y >= 296 and y < 417):
-					area = "F1"				
-				elif( y >= 417 and y < 538):
-					area = "E1"				
-				elif( y >= 538 and y < 655):			
-					area = "E2"				
-				elif( y >= 655 and y < 772):
-					area = "E3"
-				elif( y >= 772 and y < 820):
+				if( x < 40):
+					area = "D4"	
+				if( x >= 40 and x < 105.1):
+					area = "B4"
+				elif( x >= 105.1 and x < 283.35):
+					area = "A4"
+				elif( x >= 283.35 and x < 350):
+					area = "C4"
+				elif( x >= 350):
 					area = "E4"
-			elif( x > 378):
-				if( y >= 14 and y < 60):			
-					area = "E4"				
-				elif( y >= 60 and y < 178):
-					area = "E3"				
-				elif( y >= 178 and y < 296):
-					area = "E2"				
-				elif( y >= 296 and y < 417):
+			elif( y < 78.02):
+				if( x < 40):
+					area = "D3"	
+				if( x >= 40 and x < 105.1):
+					area = "B3"
+				elif( x >= 105.1 and x < 283.35):
+					area = "A3"
+				elif( x >= 283.35 and x < 350):
+					area = "C3"
+				elif( x >= 350):
+					area = "E3"
+			elif( y < 277.98):
+				if( x < 40):
+					area = "D2"	
+				if( x >= 40 and x < 105.1):
+					area = "B2"
+				elif( x >= 105.1 and x < 283.35):
+					area = "A2"
+				elif( x >= 283.35 and x < 350):
+					area = "C2"
+				elif( x >= 350):
+					area = "E2"
+			elif( y < 417):
+				if( x < 40):
+					area = "D1"	
+				if( x >= 40 and x < 105.1):
+					area = "B1"
+				elif( x >= 105.1 and x < 283.35):
+					area = "A1"
+				elif( x >= 283.35 and x < 350):
+					area = "C1"
+				elif( x >= 350):
 					area = "E1"
-				elif( y >= 417 and y < 538):
-					area = "F1"				
-				elif( y >= 538 and y < 655):
-					area = "F2"
-				elif( y >= 655 and y < 772):
-					area = "F3"				
-				elif( y >= 772 and y < 820):		
-					area = "F4"
+			elif( y < 556.02):
+				if( x < 40):
+					area = "E1"	
+				if( x >= 40 and x < 105.1):
+					area = "C1"
+				elif( x >= 105.1 and x < 283.35):
+					area = "A1"
+				elif( x >= 283.35 and x < 350):
+					area = "B1"
+				elif( x >= 350):
+					area = "D1"
+			elif( y < 755.12):
+				if( x < 40):
+					area = "E2"	
+				if( x >= 40 and x < 105.1):
+					area = "C2"
+				elif( x >= 105.1 and x < 283.35):
+					area = "A2"
+				elif( x >= 283.35 and x < 350):
+					area = "B2"
+				elif( x >= 350):
+					area = "D2"
+			elif( y < 820):
+				if( x < 40):
+					area = "E3"	
+				if( x >= 40 and x < 105.1):
+					area = "C3"
+				elif( x >= 105.1 and x < 283.35):
+					area = "A3"
+				elif( x >= 283.35 and x < 350):
+					area = "B3"
+				elif( x >= 350):
+					area = "D3"
+			elif( y >= 820):
+				if( x < 40):
+					area = "E4"	
+				if( x >= 40 and x < 105.1):
+					area = "C4"
+				elif( x >= 105.1 and x < 283.35):
+					area = "A4"
+				elif( x >= 283.35 and x < 350):
+					area = "B4"
+				elif( x >= 350):
+					area = "D4"
 
-			elif( x >= 10 and x < 40 and y >= 14 and y < 60 ):
-				area = "D4"
-			elif( x >= 10 and x < 40 and y >= 60 and y < 178 ):
-				area = "D3"
-			elif( x >= 10 and x < 40 and y >= 178 and y < 296 ):
-				area = "D2"
-			elif( x >= 10 and x < 40 and y >= 296 and y < 417 ):
-				area = "D1"
-			elif( x >= 10 and x < 40 and y >= 417 and y < 538 ):
-				area = "C1"
-			elif( x >= 10 and x < 40 and y >= 538 and y < 655 ):
-				area = "C2"
-			elif( x >= 10 and x < 40 and y >= 655 and y < 772 ):
-				area = "C3"
-			elif( x >= 10 and x < 40 and y >= 772 and y < 820 ):
-				area = "C4"
-			elif( x >= 40 and x < 195 and y >= 14 and y < 60 ):
-				area = "B4"
-			elif( x >= 40 and x < 195 and y >= 60 and y < 178 ):
-				area = "B3"
-			elif( x >= 40 and x < 195 and y >= 178 and y < 296 ):
-				area = "B2"
-			elif( x >= 40 and x < 195 and y >= 296 and y < 417 ):
-				area = "B1"
-			elif( x >= 40 and x < 195 and y >= 417 and y < 538 ):
-				area = "A1"
-			elif( x >= 40 and x < 195 and y >= 538 and y < 655 ):
-				area = "A2"
-			elif( x >= 40 and x < 195 and y >= 655 and y < 772 ):
-				area = "A3"
-			elif( x >= 40 and x < 195 and y >= 772 and y < 820 ):
-				area = "A4"
-			elif( x >= 195 and x < 350 and y >= 14 and y < 60 ):
-				area = "A4"
-			elif( x >= 195 and x < 350 and y >= 60 and y < 178 ):
-				area = "A3"
-			elif( x >= 195 and x < 350 and y >= 178 and y < 296 ):
-				area = "A2"
-			elif( x >= 195 and x < 350 and y >= 296 and y < 417 ):
-				area = "A1"
-			elif( x >= 195 and x < 350 and y >= 417 and y < 538 ):
-				area = "B1"
-			elif( x >= 195 and x < 350 and y >= 538 and y < 655 ):
-				area = "B2"
-			elif( x >= 195 and x < 350 and y >= 655 and y < 772 ):
-				area = "B3"
-			elif( x >= 195 and x < 350 and y >= 772 and y < 820 ):
-				area = "B4"
-			elif( x >= 350 and x < 378 and y >= 14 and y < 60 ):
-				area = "C4"
-			elif( x >= 350 and x < 378 and y >= 60 and y < 178 ):
-				area = "C3"
-			elif( x >= 350 and x < 378 and y >= 178 and y < 296 ):
-				area = "C2"
-			elif( x >= 350 and x < 378 and y >= 296 and y < 417 ):
-				area = "C1"
-			elif( x >= 350 and x < 378 and y >= 417 and y < 538 ):
-				area = "D1"
-			elif( x >= 350 and x < 378 and y >= 538 and y < 655 ):
-				area = "D2"
-			elif( x >= 350 and x < 378 and y >= 655 and y < 772 ):
-				area = "D3"
-			elif( x >= 350 and x < 378 and y >= 772 and y < 820 ):
-				area = "D4"
-			else:
-				area = "yy"
-				
 			#rescale coordinate
-			if x < 0: 
-				x = -1
-			elif x <= 10: 
-				x *= 36.4/10.0
+			if x <= 10: 
+				x = x-10
 			elif x <= 40: 
-				x = (x-10.0)*32.2/30.0+36.4
-			elif x <= 195: 
-				x = (x-40.0)*179.9/155.0+68.6
+				x = (x-10.0)*32.0/30.0
 			elif x <= 350: 
-				x = (x-195.0)*179.9/155.0+248.5
-			elif x <= 378: 
-				x = (x-350.0)*32.2/28.0+428.4
-			elif x <= 388: 
-				x = (x-378.0)*36.4/10.0+460.6
+				x = (x-40.0)*360.0/310.0+32.0
 			else: 
-				x = -1.0
+				x = (x-350.0)*32.0/30.0+392.0
 
-			if y <0:
-				y = -1
-			elif y <= 14: 
-				y *= 57.4/14.0
-			elif y <= 60: 
-				y = (y-14.0)*53.2/46.0+57.4
-			elif y <= 178: 
-				y = (y-60.0)*137.2/118.0+110.6
-			elif y <= 296: 
-				y = (y-178.0)*137.2/118.0+247.8
+			if y <= 14:
+				y = y-14
+			elif y <= 78.02: 
+				y = (y-14.0)*74.0/64.0
+			elif y <= 277.98: 
+				y = (y-78.02)*233.0/199.96+74.0
 			elif y <= 417: 
-				y = (y-296.0)*140.0/121.0+385.0
-			elif y <= 538: 
-				y = (y-417.0)*140.0/121.0+525.0
-			elif y <= 655: 
-				y = (y-538.0)*137.2/117.0+665.0
-			elif y <= 772: 
-				y = (y-655.0)*137.2/117.0+802.2
-			elif y <= 820: 
-				y = (y-772.0)*53.2/48.0+939.4
-			elif y <= 824: 
-				y = (y-820.0)*57.4/14.0+992.6
-			else: 
-				y = -1.0
+				y = (y-277.98)*161.0/139.02+307.0
+			elif y <= 556.02: 
+				y = (y-417.0)*161.0/139.02+468.0
+			elif y <= 755.12: 
+				y = (y-556.02)*233.0/199.96+629.0
+			else:
+				y = (y-755.12)*74.0/64.0+861.0
 
-			x = round(x-36+0.5,0)
-			y = round(y-57+0.5,0)
+			#x = round(x-36+0.5,0)
+			#y = round(y-57+0.5,0)
 
 			outfile.write(str(int(x)))
 			outfile.write(',')
